fa248751afbec10c25e167748bc38a0c070160a2
Profe Mario <mariocaresca@santotomas.cl>
2025-06-12 20:33:10 -0400

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import { Hono } from "hono";
 import playerRoute from "./routes/player.route";
 import { prettyJSON } from "hono/pretty-json";
+import { cors } from "hono/cors";
 
 const app = new Hono();
 
 app.use(prettyJSON());
+
+app.use("*", cors({
+  origin: "http://127.0.0.1:5500"
+}));
 
 app.get("/", (c) => {
   return c.text("App funcionando!");
